9d2ef4ac2fb161556ecc5663dc8be8d51a9af256
PAKISTAN <pakistan@192.168.10.63>
2023-06-23 01:05:45 +0500

    working with lights

--- modules/extra.js
+++ modules/extra.js
@@ -1,9 +1,31 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// const scene = new THREE.Scene();
 
 
 // the scene is passed in main.js where setupFloor is called
 export const setupExtra = (scene, camera, renderer, paintingNumber) => {
+
+  // var scene = new THREE.Scene();
+  // var camera2 = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000);
+  // var renderer = new THREE.WebGLRenderer();
+  // renderer.setSize(window.innerWidth, window.innerHeight);
+  // document.body.appendChild(renderer.domElement)
+
+  // var light = new THREE.HemisphereLight(0xffffff, 0x000000, 2)
+  // scene.add(light);
+
+  // const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+  // scene.add(directionalLight)
+  
+  // const pointLight = new THREE.PointLight(0xffffff, 0.3)
+  // scene.add(pointLight)
+
+  // const rectAreaLight = new THREE.RectAreaLight(0xffffff, 2, 1, 1)
+  // scene.add(rectAreaLight);
+
+
     let position = 0
   let mixer = null;
   const gltfLoader = new GLTFLoader()
@@ -19,12 +41,16 @@ export const setupExtra = (scene, camera, renderer, paintingNumber) => {
       });
       const model = gltf.scene;
     //   model.position.set(0, -1, 10)
-    // model.position.z = -15;
-    // model.position.y = -2;
+    // if(paintingNumber === "10") {
+    //   // model.position.z = 15;
+    //   model.position.y = -2;
+    //   console.log('model position', model.position);
+    // }
       model.name = 'catModel';
       console.log('model: ', model)
       gltf.scene.scale.set(5.25, 5.25, 5.25); 
-      camera.position.set(0, 0, 20);
+      // gltf.scene.scale.set(0.025, 0.025, 0.025); 
+      // camera.position.set(0, 0, 20);
       scene.add(model)
       
     },
@@ -34,9 +60,12 @@ export const setupExtra = (scene, camera, renderer, paintingNumber) => {
     () => {
       console.log("error");
     });
+
+    
+
 const clock = new THREE.Clock();
 let previousTime = 0;
-camera.position.set(0, 0, 20);
+// camera.position.set(0, 0, 20);
 const animate = () => {
     const elapsedTime = clock.getElapsedTime();
     const deltaTime = elapsedTime - previousTime;
@@ -48,8 +77,9 @@ const animate = () => {
     //renderer
     renderer.render(scene, camera);
     
-    // update controls
     // controls.update();
+
+    // console.log('This is update controls: ', controls.update());
     
     renderer.setAnimationLoop(animate);
 };
